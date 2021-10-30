075c2116e82b47422ac580910ae6ea767bb3c761
Kaique Vinicius <cyberkaidev@gmail.com>
2021-10-30 01:22:19 -0300

    :recycle: Favorites refactoring

--- lib/pages/favorites/favorites_page.dart
+++ lib/pages/favorites/favorites_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lol/components/app_bar_back.dart';
 
 class FavoritesPage extends StatefulWidget {
   const FavoritesPage({Key? key}) : super(key: key);
@@ -11,8 +12,12 @@ class _FavoritesPageState extends State<FavoritesPage> {
   
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(AppBar().preferredSize.height + 15),
+        child: const AppBarBack(name: 'Favoritos'),
+      ),
+      body: Container(),
     );
   }
 }
